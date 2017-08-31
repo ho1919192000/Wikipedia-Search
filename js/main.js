0465bf6f2c41725898c98824132e3c43248f7c7a
Howard <ho1919192000@gmail.com>
2017-08-31 16:50:44 -0600

    Add real files

--- js/main.js
+++ js/main.js
@@ -1,30 +1,50 @@
-$.ajaxSetup({
-    cache: false
-});
-if (navigator.geolocation) { //Access our user's current location. 
-    navigator.geolocation.getCurrentPosition(function (position) {
-        let lat = position.coords.latitude;
-        let lon = position.coords.longitude;
+$(document).ready(function () {
 
-        $.getJSON(`https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${lon}`, function (json) {
-            $("#location").html(json.name + ", " + json.sys.country);
-            $("#temp").html(json.main.temp);
-            $("#weather").html(json.weather[0].main);
-            $("#icon").html(`<img src="${json.weather[0].icon}"></img>`);
-            $("#tranfer").on("click", tempUnit);
+    function search() {
+        var quote = "";
 
-        });
-    });
-}
+        if (!$("#ed-srch-term").val().trim()) {
+            $("#result").html("");
+        } else {
+            quote = $("#ed-srch-term").val();
+            $.ajax({
+                url: '//en.wikipedia.org/w/api.php',
+                data: {
+                    action: 'query',
+                    list: 'search',
+                    srsearch: quote,
+                    format: 'json'
+                },
+                dataType: 'jsonp',
+                success: function (x) {
+                    var data = [];
+                    var result = "";
+                    data = x.query.search;
+                    console.log(data);
+                    for (var i = 0; i < data.length; i++) {
+                        result += `
+           <div class="card"> 
+             <div class="card-block">
+               <h4 class="card-title">${data[i].title}</h4>
+               <p class="card-text">${data[i].snippet}</p>
+               <a href="https://en.wikipedia.org/wiki/${data[i].title}" class="btn btn-primary" target="_blank">More Information</a>
+             </div>
+           </div>
+          `;
+                    }
+                    $("#result").html(result);
+                }
+            }) /*end of $.ajax()*/
+        } /*end of if(quote === undefined)*/
 
-function tempUnit() {
-    if ($("#tempUnit").html() === "C") {
-        let f = Number($("#temp").html()) * 1.8 + 32;
-        $("#temp").html(f.toFixed(2));
-        $("#tempUnit").html("F");
-    } else {
-        let c = (Number($("#temp").html()) - 32) / 1.8;
-        $("#temp").html(c.toFixed(2));
-        $("#tempUnit").html("C");
-    }
-}
+    } /*end of search()*/
+
+
+    $("#ed-srch-term").keypress(function (e) {
+        if (e.which == 13) {
+            search();
+            return false;
+        }
+    }); /*end of $("#ed-srch-term").keypress()*/
+
+}); /*end of $(document).ready()*/
